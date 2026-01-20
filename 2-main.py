dd8326625419e32b98ecfa1784de98ee81483b13
Abdulrahmon Babatunde <abdulrahmonbb@gmail.com>
2026-01-20 11:18:18 +0100

    add: 2-main.py and test

---
+++ 2-main.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+
+import random
+
+
+class DeckOfCards:
+    SUITS = ["Hearts", "Diamonds", "Clubs", "Spades"]
+    RANKS = [
+        "Ace",
+        "2",
+        "3",
+        "4",
+        "5",
+        "6",
+        "7",
+        "8",
+        "9",
+        "10",
+        "Jack",
+        "Queen",
+        "King",
+    ]
+
+    def __init__(self):
+        self.__cards = []
+        self.create_deck()
+
+    def create_deck(self):
+        for Suit in self.SUITS:
+            for Rank in self.RANKS:
+                self.__cards.append((Rank, Suit))
+
+    def shuffle_deck(self):
+        random.shuffle(self.__cards)
+
+    def deal_card(self):
+        if len(self.__cards) == 0:
+            return None
+        return self.__cards.pop()
+
+    # don't touch below this line
+
+    def __str__(self):
+        return f"The deck has {len(self.__cards)} cards"
